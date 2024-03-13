c1a3ef4fe881e103be1e99f024e0d167eb71bbc6
segyu <skrheem1116@gmail.com>
2024-03-13 16:42:01 +0900

    Add files via upload

---
+++ ScannerDemo.java
@@ -0,0 +1,15 @@
+package chap02;
+import java.util.Scanner;
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int x = in.nextInt();
+		int y = in.nextInt();
+		System.out.printf("%d * %d은 %d입니다.\n", x, y, x * y);
+		System.out.println(x++);
+		System.out.println(x);
+
+	}
+
+}
---
+++ VarDemo.java
@@ -0,0 +1,16 @@
+package chap02;
+
+public class VarDemo {
+//	var a = 1;
+	public static void main(String[] args) {
+		int var = 1;
+		var x = 1;
+		
+		// var x = 1, y = 3, z = 4; var을 사용하여 다수의 변수를 동시에 초기화할 수 없다.
+		// var str = null; var 변수에는 null 값을 대입할 수 없다.
+		//var oops; 변수를 바로 초기화하지 않으면
+		//oops = 1; 타입이 뭔지 모른다.
+
+	}
+//void test(var x) {} 타입이 뭔지 모름
+}
